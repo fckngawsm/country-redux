80f343677f69b279ae6c3e29d51d1af09c367ca9
Kirill <forworkskirill@gmail.com>
2023-02-25 19:12:29 +0500

    create logic for redux

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { rootReducer } from "./root-reducer";
+import * as api from '../config'
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
+
+export const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(
+      thunk.withExtraArgument({
+        client: axios,
+        api
+      })
+    )
+  )
+);
